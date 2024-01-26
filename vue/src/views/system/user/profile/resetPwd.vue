2b2ad71f78c71f23e050e6d04a3fcfc9d7aae13b
Zhu <2698076424@qq.com>
2024-01-26 16:36:31 +0800

    fix(个人中心模块): 修改密码后退出系统

--- vue/src/views/system/user/profile/resetPwd.vue
+++ vue/src/views/system/user/profile/resetPwd.vue
@@ -1,13 +1,13 @@
 <template>
   <el-form ref="form" :model="user" :rules="rules" label-width="80px">
     <el-form-item label="旧密码" prop="oldPassword">
-      <el-input v-model="user.oldPassword" placeholder="请输入旧密码" type="password" show-password/>
+      <el-input v-model="user.oldPassword" placeholder="请输入旧密码" type="password" show-password />
     </el-form-item>
     <el-form-item label="新密码" prop="newPassword">
-      <el-input v-model="user.newPassword" placeholder="请输入新密码" type="password" show-password/>
+      <el-input v-model="user.newPassword" placeholder="请输入新密码" type="password" show-password />
     </el-form-item>
     <el-form-item label="确认密码" prop="confirmPassword">
-      <el-input v-model="user.confirmPassword" placeholder="请确认新密码" type="password" show-password/>
+      <el-input v-model="user.confirmPassword" placeholder="请确认新密码" type="password" show-password />
     </el-form-item>
     <el-form-item>
       <el-button type="primary" size="mini" @click="submit">保存</el-button>
@@ -55,7 +55,15 @@ export default {
       this.$refs["form"].validate(valid => {
         if (valid) {
           updateUserPwd(this.user.oldPassword, this.user.newPassword).then(response => {
-            this.$modal.msgSuccess("修改成功");
+            console.log(response);
+            this.$modal.msgSuccess("修改成功，请重新登录！");
+            if (response.code == 200) {
+              //清除登录缓存
+              localStorage.removeItem('token');
+              this.$store.dispatch('LogOut').then(() => {
+                location.href = '/index';
+              })
+            }
           });
         }
       });
